67a8358eff7518674dcfc64578dde76e0401ae15
Richard Mohammed <richardamod@hotmail.com>
2021-12-07 21:50:59 +0000

    Day 5 first part

--- AoC2021/Day5.cs
+++ AoC2021/Day5.cs
@@ -1,7 +1,117 @@
-﻿namespace AoC2021
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace AoC2021
 {
     public class Day5
     {
-        
+        public static async Task<List<List<CoOrdinate>>> GetValues(string[] sourceData = null)
+        {
+           var data = sourceData ?? await SourceDataHelper.GetDataFromUrl("/2021/day/5/input").ConfigureAwait(false);
+
+           var values = data.Select(x => x.Replace(" ", "").Split(new string[] {"->"}, StringSplitOptions.None)).ToList();
+           var coOrdinates = values.Select(c => new List<CoOrdinate>{
+               new CoOrdinate
+               {
+                   Col = int.Parse(c[0].Split(',')[0]),
+                   Row = int.Parse(c[0].Split(',')[1])
+               },
+               new CoOrdinate
+               {
+                   Col = int.Parse(c[1].Split(',')[0]),
+                   Row = int.Parse(c[1].Split(',')[1])
+               }
+           }).ToList();
+
+           return coOrdinates;
+        }
+
+        public static List<List<CoOrdinate>> GetFilteredValues(IEnumerable<List<CoOrdinate>> coOrdinates) =>
+            coOrdinates.Where(c => c.First().Col == c.Last().Col || c.First().Row == c.Last().Row).ToList();
+
+        public static async Task<int[,]> PlotMatrix(List<List<CoOrdinate>> data = null)
+        {
+            var allCoOrds = data ?? await GetValues().ConfigureAwait(false);
+            var coOrds = GetFilteredValues(allCoOrds);
+            var maxRows = coOrds.SelectMany(x => x.Select(c => c.Row)).Max() + 1;
+            var maxCols = coOrds.SelectMany(x => x.Select(c => c.Col)).Max() + 1;
+
+            var matrix = new int[maxRows, maxCols];
+
+            foreach (var lineCoOrd in coOrds)
+            {
+                var from = lineCoOrd.First();
+                var to = lineCoOrd.Last();
+
+                if (from.Row == to.Row)
+                {
+                    // draw vertical line
+                    if (from.Col < to.Col)
+                    {
+                        for (var i = from.Col; i <= to.Col; i++)
+                        {
+                            matrix[from.Row, i] += 1;
+                        }
+                    }
+                    else
+                    {
+                        for (var i = from.Col; i >= to.Col; i--)
+                        {
+                            matrix[from.Row, i] += 1;
+                        }
+                    }
+                }
+                else
+                {
+                    //draw horizontal line
+                    if (from.Row < to.Row)
+                    {
+                        for (var i = from.Row; i <= to.Row; i++)
+                        {
+                            matrix[i, from.Col] += 1;
+                        }
+                    }
+                    else
+                    {
+                        for (var i = from.Row; i >= to.Row; i--)
+                        {
+                            matrix[i, from.Col] += 1;
+                        }
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        public static async Task<int> CalculateDangerousPoints(int[,] matrix = null)
+        {
+            var dangerousPoints = 0;
+            matrix = matrix ?? await PlotMatrix().ConfigureAwait(false);
+
+            var matrixRowCount = matrix.GetLength(0);
+            var matrixColCount = matrix.GetLength(1);
+            for (var row = 0; row < matrixRowCount; row++)
+            {
+                for (var col = 0; col < matrixColCount; col++)
+                {
+                    if (matrix[row, col] > 1)
+                    {
+                        dangerousPoints++;
+                    }
+                }
+            }
+
+            return dangerousPoints;
+        }
+    }
+
+    public class CoOrdinate
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
     }
 }
---
+++ AoC2021_Test/Day5Test.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AoC2021;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2021_Test
+{
+    public class Day5Test
+    {
+        private ITestOutputHelper _testOutputHelper;
+        private readonly string[] _data = new []{ "0,9 -> 5,9", "8,0 -> 0,8", "9,4 -> 3,4", "2,2 -> 2,1", "7,0 -> 7,4", "6,4 -> 2,0", "0,9 -> 2,9", "3,4 -> 1,4", "0,0 -> 8,8", "5,5 -> 8,2"};
+
+        public Day5Test(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task GetDataTest_ReturnsCoOrdinates()
+        {
+            var coOrds = await Day5.GetValues().ConfigureAwait(false);
+
+            foreach (var item in coOrds)
+            {
+                _testOutputHelper.WriteLine($"{item.First().Row},{item.First().Col} {item.Last().Row},{item.Last().Col}");
+            }
+        }
+
+        [Fact]
+        public async Task PlotMatrixTest_ReturnsValues()
+        {
+            var testData = await Day5.GetValues(_data).ConfigureAwait(false);
+            var matrix = await Day5.PlotMatrix(testData).ConfigureAwait(false);
+
+            // assert rows x columns rather than X x Y
+            Assert.Equal(2, matrix[9, 0]);
+
+            for (var r = 0; r < matrix.GetLength(0); r++)
+            {
+                var row = "";
+                for (var c = 0; c < matrix.GetLength(1); c++)
+                {
+                    row += $"{matrix[r, c]} ";
+
+                }
+                _testOutputHelper.WriteLine(row);
+            }
+            _testOutputHelper.WriteLine( matrix[0, 9].ToString());
+
+        }
+
+        [Fact]
+        public async Task CalculateDangerousPointsTest_ReturnsValues()
+        {
+            var testData = await Day5.GetValues(_data).ConfigureAwait(false);
+            var matrix = await Day5.PlotMatrix(testData).ConfigureAwait(false);
+            var numDangerousPoints = await Day5.CalculateDangerousPoints(matrix).ConfigureAwait(false);
+
+            Assert.Equal(5, numDangerousPoints);
+            _testOutputHelper.WriteLine(numDangerousPoints.ToString());
+        }
+
+        [Fact]
+        public async Task PlotReadDataMatrixTest_ReturnsValues()
+        {
+            var matrix = await Day5.PlotMatrix().ConfigureAwait(false);
+
+            for (var r = 0; r < matrix.GetLength(0); r++)
+            {
+                var row = "";
+                for (var c = 0; c < matrix.GetLength(1); c++)
+                {
+                    row += $"{matrix[r, c]} ";
+
+                }
+                _testOutputHelper.WriteLine(row);
+            }
+            _testOutputHelper.WriteLine( matrix[0, 9].ToString());
+
+        }
+
+        [Fact]
+        public async Task CalculateDangerousPointsFromRealDataTest_ReturnsValues()
+        {
+            var numDangerousPoints = await Day5.CalculateDangerousPoints().ConfigureAwait(false);
+
+            _testOutputHelper.WriteLine(numDangerousPoints.ToString());
+        }
+    }
+}
